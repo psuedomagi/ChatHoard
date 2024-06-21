783e2a2c6a05e03044bd9cec0b6fa9e9682876f7
psuedomagi <githubby@impulsecurve.com>
2024-06-20 21:55:41 -0400

    removed bcrypt dependency in favor of argon2

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -6,17 +6,17 @@ from passlib.context import CryptContext
 
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from datetime import timezone
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 ALGORITHM = "HS256"
 
 
 def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
-    expire = datetime.utcnow() + expires_delta
+    expire = datetime.now(timezone.utc) + expires_delta
     to_encode = {"exp": expire, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
